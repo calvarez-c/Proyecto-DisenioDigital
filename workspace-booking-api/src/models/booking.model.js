40166c99524caaa39df6dc8f40e6f49f106ad251
jose.mejia <daniel.cuellar@unah.hn>
2026-08-01 17:12:20 -0600

    creacion de reservas y metodo para obtener ultima reserva creada

--- workspace-booking-api/src/models/booking.model.js
+++ workspace-booking-api/src/models/booking.model.js
@@ -24,5 +24,70 @@ export class BookingModel {
         )
         return myBookings
     }
+
+
+    static async getLastBooking(user_id) {
+        await using conn = await pool.getConnection()       
+
+        const [lastBooking] = await conn.execute(
+            `   SELECT id, user_id, resource_id, start_time, end_time, 
+                total_price, status, created_at
+                FROM bookings WHERE user_id = ?
+                ORDER BY created_at DESC LIMIT 1    `,
+            [user_id]
+        )
+        return lastBooking
+    }
+
+
+
+
+    static async create( user_id, resource_id, start_time, end_time, total_price) {
+        console.log(user_id, resource_id, start_time, end_time, total_price);
+        
+        await using conn = await pool.getConnection()
+        const [result] = await conn.execute(
+            `   INSERT INTO bookings (user_id, resource_id, start_time, end_time, total_price) 
+                VALUES (?, ?, ?, ?, ?)`,
+            [user_id, resource_id, start_time, end_time, total_price]
+        )
+        return result
+    }
+
+
+    static async validateOverlap(resource_id, start_time,end_time ) {
+
+        await using conn = await pool.getConnection()
+        const [result] = await conn.execute(
+            `   SELECT id
+                FROM bookings 
+                WHERE status = 'CONFIRMED' AND resource_id = ?
+                AND start_time < ?
+                AND end_time > ?    `,
+            [resource_id, end_time, start_time ]
+        )
+        return result[0]
+    }
+
+
+
+
+
+
+
+
+
+
+
+    //TODO >> quitar este metodo ya que es temporal mientras se termina el modelo de los recursos
+    static async getResource(resource_id) {
+        await using conn = await pool.getConnection()       
+
+        const [resource] = await conn.execute(
+            `   SELECT  * FROM resources WHERE id = ?    `,
+            [resource_id]
+        )
+        return resource[0]
+    }
     
 }
