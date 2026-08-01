ff042c47e21a6eacc92a2c534ac6e31d36c0948c
jose.mejia <daniel.cuellar@unah.hn>
2026-08-01 16:04:37 -0600

    validacion de fechas en controlador de reserva

--- workspace-booking-api/src/controllers/booking.controller.js
+++ workspace-booking-api/src/controllers/booking.controller.js
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken'
 import { jsonResponse } from '../helpers/json_response.js'
 import { BookingModel } from '../models/booking.model.js'
+import { validateBooking } from '../schemas/booking.schema.js'
+import { start } from 'node:repl'
 
 export class BookingController {
 
@@ -44,6 +46,37 @@ export class BookingController {
             return res.status(500).json(jsonResponse(500, "Error interno del servidor", error.message))
         }
     }
+
+
+
+
+    static async createBooking(req, res) {
+            const result = validateBooking(req.body) //TODO >> validar body con zod
+
+            if (!result.success) {
+                return res.status(400).json(jsonResponse(400, result.error.errors || result.error.issues))
+            }
+                
+            const  resource_id  = result.data.resource_id
+            const  start_time  = new Date(result.data.start_time)
+            const  end_time  = new Date(result.data.end_time)
+
+
+            if (end_time < start_time  || start_time < new Date() ) {
+                return res.status(400).json(jsonResponse(400, "La fecha de inicio debe ser menor que la fecha de fin y no puede ser una fecha pasada"))                
+            }
+
+
+            try {
+
+                
+                return res.status(201).json(jsonResponse(201, "éxito"))
+            
+            
+            } catch (error) {
+                return res.status(500).json(jsonResponse(500, "Error interno del servidor", error.message))
+            }
+        }
 
     
 
