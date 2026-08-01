dbbd08bb18de0fd59728baeaf96f8e6868ab442f
jose.mejia <daniel.cuellar@unah.hn>
2026-08-01 15:01:56 -0600

    controlador para obtener todas las reservas validando que sea admin

--- workspace-booking-api/src/controllers/booking.controller.js
+++ workspace-booking-api/src/controllers/booking.controller.js
@@ -4,6 +4,26 @@ import { BookingModel } from '../models/booking.model.js'
 
 export class BookingController {
 
+    static async getAllBookings(req, res) {
+        
+        const {id, role} = req.user
+    
+        try{
+
+            if (role !== 'ADMIN') {
+                return res.status(403).json(jsonResponse(403, "No tiene acceso al recurso"))
+            }
+
+            const bookings = await BookingModel.getAllBookings()
+
+            return res.status(200).json(jsonResponse(200, `Todas las reservas`, bookings))
+        
+        } catch (error) {            
+            return res.status(500).json(jsonResponse(500, "Error interno del servidor", error.message))
+        }
+    }
+
+
 
     static async getMyBookings(req, res) {
         
