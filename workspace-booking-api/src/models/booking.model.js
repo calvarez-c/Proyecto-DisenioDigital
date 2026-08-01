4f68ad5193640ee7ca2494a47c2a41f4eacc81c2
jose.mejia <daniel.cuellar@unah.hn>
2026-08-01 14:42:42 -0600

    corregir tabla

--- workspace-booking-api/src/models/booking.model.js
+++ workspace-booking-api/src/models/booking.model.js
@@ -3,13 +3,16 @@ import { pool } from '../config/db.js'
 export class BookingModel {
 
 
-    static async getMyBookings({ user_id }) {
+    static async getMyBookings(user_id) {
         await using conn = await pool.getConnection()
         
+        console.log(user_id);
+        
+
         const [myBookings] = await conn.execute(
             `   SELECT  id, user_id, resource_id, start_time, end_time, 
                 total_price, status, created_at
-                FROM users WHERE user_id = ?    `,
+                FROM bookings WHERE user_id = ?    `,
             [user_id]
         )
         return myBookings
