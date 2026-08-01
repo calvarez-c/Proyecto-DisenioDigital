533bbfd5d2f4e76f524032db7febd86c6dbf8642
jose.mejia <daniel.cuellar@unah.hn>
2026-08-01 14:20:40 -0600

    metodo para obtener los bookings de un usuario (cliente)

---
+++ workspace-booking-api/src/models/booking.model.js
@@ -0,0 +1,18 @@
+import { pool } from '../config/db.js'
+
+export class BookingModel {
+
+
+    static async getMyBookings({ user_id }) {
+        await using conn = await pool.getConnection()
+        
+        const [myBookings] = await conn.execute(
+            `   SELECT  id, user_id, resource_id, start_time, end_time, 
+                total_price, status, created_at
+                FROM users WHERE user_id = ?    `,
+            [user_id]
+        )
+        return myBookings
+    }
+    
+}
