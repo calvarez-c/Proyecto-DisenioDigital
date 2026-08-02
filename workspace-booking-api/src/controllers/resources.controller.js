b949411a5fb7f3aa602e7188bf6d9cf32dc5e7c5
Bryan01Cruz <bcruzi@unah.hn>
2026-08-02 13:10:46 -0600

    Creación del método createResource en el controller y validación de usuario.

--- workspace-booking-api/src/controllers/resources.controller.js
+++ workspace-booking-api/src/controllers/resources.controller.js
@@ -12,4 +12,24 @@ export class ResourceController {
             return res.status(500).json(jsonResponse(500,"Error interno del servidor.", error.message))
         }
     }
+
+    static async createResource(req, res) {
+        const validation = validateCreateResource(req.body)
+
+        if (!validation.success) {
+            return res.status(400).json(jsonResponse(400, validation.error.errors || validation.error.issues))
+        }
+
+        const {role} = req.user
+        if (role !== 'ADMIN') {
+            return res.status(403).json(jsonResponse(403, "No tienes permisos para realizar esta acción."))
+        }
+
+        try {
+            const id = await ResourceModel.create(validation.data)
+            return res.status(201).json(jsonResponse(201, "Recurso creado exitosamente.", {id}))
+        }catch (error) {
+            return res.status(500).json(jsonResponse(500, "Error interno del servidor.", error.message))
+        }
+    }
 }
