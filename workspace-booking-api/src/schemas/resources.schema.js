ff138a773568416712d0e4efc75ba730d333004e
Bryan01Cruz <bcruzi@unah.hn>
2026-08-02 12:23:05 -0600

    Creación de esquema para la validación del método update y realización de la exportación de los mismos.

--- workspace-booking-api/src/schemas/resources.schema.js
+++ workspace-booking-api/src/schemas/resources.schema.js
@@ -6,3 +6,14 @@ const createResourceSchema = z.object({
     capacity: z.number().int().positive("Este dato debe ser un número positivo."),
     price_per_hour: z.number().positive("El precio debe ser un número posivivo.")
 }).strict()
+
+const updateResourceSchema = z.object({
+    name: z.string().min(3, "El nombre debe tener por lo menos 3 caracteres.").optional(),
+    description: z.string().optional(),
+    capacity: z.number().int().positive("Este dato deber ser un número positivo.").optional(),
+    price_per_hour: z.number().positive("El precio debe ser un número posivivo.").optional(),
+    is_active: z.boolean().optional()
+}).strict()
+
+export const vallidateCreateResource = (data) => createResourceSchema.safeParse(data)
+export const validateUpdateResource = (data) => updateResourceSchema.safeParse(data)
